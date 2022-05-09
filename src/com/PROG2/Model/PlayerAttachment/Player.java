e50a0613720276e70f72a168fbd76a740c7bad18
Niclas <rieckersniclas@gmail.com>
2022-05-09 12:41:59 +0200

    Player 1.0

--- src/com/PROG2/Model/PlayerAttachment/Player.java
+++ src/com/PROG2/Model/PlayerAttachment/Player.java
@@ -1,4 +1,11 @@
 package com.PROG2.Model.PlayerAttachment;
 
 public class Player {
+    private double level;
+    private final String name;
+
+    public Player(double level, String name) {
+        this.level = level;
+        this.name = name;
+    }
 }
