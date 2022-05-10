06e1120ce72adea51a7ec2bbf647143aebbe0393
mark <mark.fischer0404@gmail.com>
2022-05-11 01:03:09 +0200

    event 1.3

--- src/com/PROG2/Model/Events/GameEvent.java
+++ src/com/PROG2/Model/Events/GameEvent.java
@@ -1,5 +1,24 @@
 package com.PROG2.Model.Events;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.Random;
+
 public class GameEvent {
 
+    private ObservableList<Trader> trader = FXCollections.observableArrayList();
+    private int upperbound = 3;
+
+    public void randomEvent(){
+        Random randNum = new Random();
+        int randy = randNum.nextInt(upperbound);
+
+        switch(randy){
+            case 1: System.out.println("event 1");
+            case 2: System.out.println("event 2");
+            case 3: System.out.println("Trader Event");
+            default: ;
+        }
+    }
 }
