e21eb31c270a83f07629296a418f76b9648a0656
Niclas <rieckersniclas@gmail.com>
2022-05-11 00:40:34 +0200

    Player Change 1.2

--- src/com/PROG2/Model/PlayerAttachment/DungeonMaster.java
+++ src/com/PROG2/Model/PlayerAttachment/DungeonMaster.java
@@ -1,6 +1,11 @@
 package com.PROG2.Model.PlayerAttachment;
 
 
+import com.PROG2.Model.Database.ClassTypes;
+
 public class DungeonMaster extends Player{
+    public DungeonMaster(double level, double mana, double health, double hitDamage, ClassTypes classesType, int drawLimit, int zen) {
+        super(level, mana, health, hitDamage, classesType, drawLimit, zen);
+    }
     //TODO hälfte der class types gehört dem dm, andere dem player
 }
