2ab832719bdd8c8acbc0e5578daef79946b36804
mark <mark.fischer0404@gmail.com>
2022-05-13 10:31:33 +0200

    rechtschreib fehler behoben :)

--- src/com/PROG2/Model/PlayerAttachment/Adventurer.java
+++ src/com/PROG2/Model/PlayerAttachment/Adventurer.java
@@ -2,9 +2,9 @@ package com.PROG2.Model.PlayerAttachment;
 
 import com.PROG2.Model.Database.ClassTypes;
 
-public class Adventure extends Player{
+public class Adventurer extends Player{
 
-    public Adventure(double level, double mana, double health, double hitDamage, ClassTypes classesType, int drawLimit, int zen) {
+    public Adventurer(double level, double mana, double health, double hitDamage, ClassTypes classesType, int drawLimit, int zen) {
         super(level, mana, health, hitDamage, classesType, drawLimit, zen);
     }
 }
