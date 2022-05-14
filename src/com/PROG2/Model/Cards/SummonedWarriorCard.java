a345f9925ae0fa25e3809b6c6ff7abd3f1b1d4c5
mark <mark.fischer0404@gmail.com>
2022-05-15 00:02:12 +0200

    weitere kommentare, ACHTUNG ES FEHLEN NOCH EINIGE ALSO NICLAS REIN IN DIE MATILDA (in deinen klassen)

--- src/com/PROG2/Model/Cards/SummonedWarriorCard.java
+++ src/com/PROG2/Model/Cards/SummonedWarriorCard.java
@@ -5,6 +5,10 @@ import com.PROG2.Model.PlayerAttachment.Ability;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/**
+ * Klasse fürs erzeugen eines SummonedWarriorCard Objektes. Erbt von Klasse Usable, da SummonedWarriorCard zu
+ * den vom Spieler nutzbaren Items gehört.
+ */
 public class SummonedWarriorCard extends Usable {
 
     private double health;
@@ -16,6 +20,18 @@ public class SummonedWarriorCard extends Usable {
     private final boolean canSummon;
     private final int buyCost;
 
+    /**
+     * Konstruktor für SummonedWarriorCard
+     * @param ID
+     * @param health
+     * @param hitDamage
+     * @param defense
+     * @param abilities
+     * @param hitRange
+     * @param moveRange
+     * @param canSummon
+     * @param buyCost
+     */
     public SummonedWarriorCard(int ID, double health, double hitDamage, double defense, ObservableList<Ability> abilities, int hitRange, int moveRange, boolean canSummon, int buyCost) {
         super(ID);
         this.health = health;
@@ -28,6 +44,12 @@ public class SummonedWarriorCard extends Usable {
         this.buyCost = buyCost;
     }
 
+    /**
+     * Methode für das anfügen eines Effektes an die WarriorCard.
+     * Ein Effekt kann die stärke der Karte mithilfe des hinzufügen einer Ability beeinflussen.
+     * @param ability
+     * @return ability
+     */
     public SummonedWarriorCard addeffect(Ability ability){
         abilities.add(ability);
         this.active(ability);
@@ -39,54 +61,107 @@ public class SummonedWarriorCard extends Usable {
         return this;
     }
 
+    /**
+     * Getter, welcher den Health Status der Warrior Card zurückgibt.
+     * @return health
+     */
     public double health() {
         return health;
     }
 
+    /**
+     * Setter für Health der SummonedWarriorCard
+     * @param health
+     * @return health
+     */
     public SummonedWarriorCard setHealth(double health) {
         this.health = health;
         return this;
     }
 
+    /**
+     * Getter für HitDamage
+     * @return hitDamage
+     */
     public double hitDamage() {
         return hitDamage;
     }
 
+    /**
+     * Setter für HitDamage
+     * @param hitDamage
+     * @return hitDamage
+     */
     public SummonedWarriorCard setHitDamage(double hitDamage) {
         this.hitDamage = hitDamage;
         return this;
     }
 
+    /**
+     * Getter für Defense
+     * @return defense
+     */
     public double defense() {
         return defense;
     }
 
+
+    /**
+     * Setter für Defense
+     * @param defense
+     * @return defense
+     */
     public SummonedWarriorCard setDefense(double defense) {
         this.defense = defense;
         return this;
     }
 
+    /**
+     * Getter für Abilities
+     * @return ObservableList mit abilities
+     */
     public ObservableList<Ability> abilities() {
         return abilities;
     }
 
+    /**
+     * Setter für Abilities
+     * @param abilities
+     * @return ObservableList mit abilities
+     */
     public SummonedWarriorCard setAbilities(ObservableList<Ability> abilities) {
         this.abilities = abilities;
         return this;
     }
 
+    /**
+     * Getter für hitRange
+     * @return hitRange
+     */
     public int hitRange() {
         return hitRange;
     }
 
+    /**
+     * Getter für moveRange
+     * @return moveRange
+     */
     public int moveRange() {
         return moveRange;
     }
 
+    /**
+     * Getter für den canSummon boolean. Muss True sein, damit eine Karte beschworen werden kann.
+     * @return canSummon boolean
+     */
     public boolean canSummon() {
         return canSummon;
     }
 
+    /**
+     * Getter für die Kaufkosten der WarriorCard
+     * @return buyCost
+     */
     public int buyCost() {
         return buyCost;
     }
