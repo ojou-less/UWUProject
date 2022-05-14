d86c01bec79758886013046920e76c02e9e2ed4f
mark <mark.fischer0404@gmail.com>
2022-05-14 21:54:11 +0200

    weitere kommentare, ACHTUNG ES FEHLEN NOCH EINIGE ALSO NICLAS REIN AN DIE MATILDA

--- src/com/PROG2/Model/Events/Trader.java
+++ src/com/PROG2/Model/Events/Trader.java
@@ -13,18 +13,28 @@ import javafx.collections.ObservableList;
  * Klasse für den im Spiel zufällig erscheinenden Händler mit zufälligen Items, die gekauft werden können.
  */
 public class Trader {
-    private int[] IDs;
-    private Deck cardsForSale = new Deck();
+    private int[] IDs; //int array mit IDs
+    private Deck cardsForSale = new Deck(); //neues Deck wird erzeugt für den Verkauf von Karten
+    //Instanzen von ObservableList werden erstellt
     private ObservableList<Artifact> artifactsForSale = FXCollections.observableArrayList();
     private ObservableList<Weapon> weaponsForSale = FXCollections.observableArrayList();
     private ObservableList<Armor> armorForSale = FXCollections.observableArrayList();
 
 
+    /**
+     * Konstruktor für Trader
+     * @param IDs
+     */
     public Trader(int[] IDs) {
         this.IDs = IDs;
         fillTrader();
     }
 
+    /**
+     * Methode für das befüllen des Shops des Händlers
+     * Später ganz praktisch für die GUI
+     * @return
+     */
     private Trader fillTrader() {
         for (int i:
              this.IDs) {
@@ -39,17 +49,34 @@ public class Trader {
         return this;
     }
 
+    /**
+     * Getter Observable List Weapon
+     * @return Liste mit weaponsForSale
+     */
     public ObservableList<Weapon> getWeaponsForSale(){
         return weaponsForSale;
     }
+
+    /**
+     * Getter Observable List Artifacts
+     * @return Liste mit artifactsForSale
+     */
     public ObservableList<Artifact> getArtifactsForSale(){
         return artifactsForSale;
     }
 
+    /**
+     * Getter Observable List Armor
+     * @return Liste mit armorForSale
+     */
     public ObservableList<Armor> armorForSale() {
         return armorForSale;
     }
 
+    /**
+     * Getter Observable List Deck
+     * @return Liste mit cardsForSale
+     */
     public Deck getCardsForSale(){
         return cardsForSale;
     }
--- src/com/PROG2/Model/PlayerAttachment/DungeonMaster.java
+++ src/com/PROG2/Model/PlayerAttachment/DungeonMaster.java
@@ -7,15 +7,36 @@ import com.PROG2.Model.Database.ClassTypes;
  * Spielbare Chrakter "Adventurer". Dungeon Master unterscheidet sich von Adventurer in den Class Types.
  */
 public class DungeonMaster extends Player{
+    /**
+     * Konstruktor für DungeonMaster
+     * @param level
+     * @param mana
+     * @param health
+     * @param hitDamage
+     * @param classesType
+     * @param drawLimit
+     * @param zen
+     */
     public DungeonMaster(double level, double mana, double health, double hitDamage, ClassTypes classesType, int drawLimit, int zen) {
         super(level, mana, health, hitDamage, classesType, drawLimit, zen);
     }
 
+    /**
+     * Getter für die Klassentypen
+     * Wird hier überlagert, da DungeonMaster und Adventurer verschiedene Klassentypen haben sollen.
+     * @return classesType
+     */
     @Override
     public ClassTypes getClassesType() {
         return getClassesType();
     }
 
+    /**
+     * Setter für Klassentypen des DungeonMaster
+     * Wird hier überlagert, da DungeonMaster und Adventurer verschiedene Klassentypen haben sollen.
+     * @param classesType
+     * @return classesType
+     */
     public Player setClassesType(ClassTypes classesType) {
         this.setClassesType(classesType);
         return this;
