5683b39ace44de9dc787b9b4c1b9ff34d03c3588
mark <mark.fischer0404@gmail.com>
2022-05-15 00:22:10 +0200

    Projekt Struktur 1.8 mit Test Ordner (+mehr kommentare)

---
+++ junit/com/PROG2/Model/Cards/EffectCardTest.java
@@ -0,0 +1,32 @@
+package com.PROG2.Model.Cards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EffectCardTest {
+
+    @Test
+    void cardAbility() {
+    }
+
+    @Test
+    void setCardAbility() {
+    }
+
+    @Test
+    void buyCost() {
+    }
+
+    @Test
+    void setBuyCost() {
+    }
+
+    @Test
+    void summonCost() {
+    }
+
+    @Test
+    void setSummonCost() {
+    }
+}
