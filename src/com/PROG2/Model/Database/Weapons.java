c2d50d815a27e798593123146cf80bb0ce659057
Ali <h3rzius@gmail.com>
2022-05-15 16:41:13 +0200

    Weapon 1.5.7 Database Weapons reorganized ID

--- src/com/PROG2/Model/Database/Weapons.java
+++ src/com/PROG2/Model/Database/Weapons.java
@@ -11,19 +11,14 @@ import javafx.collections.ObservableList;
 public class Weapons {
     private static ObservableList<Weapon> weapons = FXCollections.emptyObservableList();
     // ----- List-Weapons
-    // Swords
     private final static Weapon swordOne = new Weapon(400,"Iron Sword", WeaponType.SWORD,10, 1,50);
     private final static Weapon swordTwo = new Weapon(401,"Majestic Sword", WeaponType.SWORD,15, 1, 200);
     private final static Weapon swordThree = new Weapon(402,"Ezclak", WeaponType.SWORD,14, 1, 180);
     private final static Weapon swordFour = new Weapon(403,"Dungeon Excalibur", WeaponType.SWORD,20, 1, 666);
-    // Bows 421-436
-    private final static Weapon bowOne = new Weapon(421, "Classical Bow", WeaponType.BOW, 8, 1, 50);
-    // Axes 437-457
-    private final static Weapon axeOne = new Weapon(437, "Housemade Axe", WeaponType.AXE, 15, 0.6, 50);
-    // Spears 458-473
-    private final static Weapon spearOne = new Weapon(458, "Wood Spear", WeaponType.SPEAR, 10, 0.8, 50);
-    // Clubs 474-489
-    private final static Weapon clubOne = new Weapon(474, "Orge Club", WeaponType.CLUB, 9, 0.6, 40);
+    private final static Weapon bowOne = new Weapon(404, "Classical Bow", WeaponType.BOW, 8, 1, 50);
+    private final static Weapon axeOne = new Weapon(405, "Housemade Axe", WeaponType.AXE, 15, 0.6, 50);
+    private final static Weapon spearOne = new Weapon(406, "Wood Spear", WeaponType.SPEAR, 10, 0.8, 50);
+    private final static Weapon clubOne = new Weapon(407, "Orge Club", WeaponType.CLUB, 9, 0.6, 40);
     //----- End
 
     public Weapons() {
