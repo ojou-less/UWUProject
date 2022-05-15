eeb6782a059112408346805e9df9bf767fff6d8d
mark <mark.fischer0404@gmail.com>
2022-05-15 17:22:25 +0200

    JUnit EffectCard feddisch

--- junit/com/PROG2/Model/Cards/EffectCardTest.java
+++ junit/com/PROG2/Model/Cards/EffectCardTest.java
@@ -1,5 +1,6 @@
 package com.PROG2.Model.Cards;
 
+import com.PROG2.Model.PlayerAttachment.Ability;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,25 +9,41 @@ class EffectCardTest {
 
     @Test
     void cardAbility() {
+        EffectCard ec = new EffectCard(1, new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4), 69, 420);
+        ec.setCardAbility(new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4));
+        assertTrue(ec.cardAbility() != null);
     }
 
     @Test
     void setCardAbility() {
+        EffectCard ec = new EffectCard(1, new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4), 69, 420);
+        ec.setCardAbility(new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4));
+        assertTrue(ec.cardAbility() != null);
     }
 
     @Test
     void buyCost() {
+        EffectCard ec = new EffectCard(1, new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4), 69, 420);
+        assertTrue(ec.buyCost() == 69);
     }
 
     @Test
     void setBuyCost() {
+        EffectCard ec = new EffectCard(1, new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4), 69, 420);
+        ec.setBuyCost(70);
+        assertTrue(ec.buyCost() == 70);
     }
 
     @Test
     void summonCost() {
+        EffectCard ec = new EffectCard(1, new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4), 69, 420);
+        assertTrue(ec.summonCost() == 420);
     }
 
     @Test
     void setSummonCost() {
+        EffectCard ec = new EffectCard(1, new Ability(1, 2, "der name", "der iconpath", 1.1, 1.2, 1.3, 1.4,4), 69, 420);
+        ec.setSummonCost(421);
+        assertTrue(ec.summonCost() == 421);
     }
 }
