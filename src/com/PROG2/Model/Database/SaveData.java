635c5f574a62e0c9a6e4e35e0ff6e86c2d8c637b
mark <mark.fischer0404@gmail.com>
2022-05-16 23:42:50 +0200

    kommis in save

--- src/com/PROG2/Model/Database/SaveData.java
+++ src/com/PROG2/Model/Database/SaveData.java
@@ -6,19 +6,24 @@ import com.PROG2.Model.PlayerAttachment.Player;
 import java.io.*;
 
 /**
-    Ist noch ungetestet
-    bei weiteren zu speichernden objekten einfach hinzufügen
-    und serializable implementieren
+ * Klasse für das Speichern und Serialisieren des Zustands des Objekts "Player", sowie das
+ * "Laden" eines Zustands aus persistentem Speicher.
  */
 public class SaveData implements Serializable {
 
+    /**
+     * Methode zum Speichern des aktuellen Zustands von Player.
+     * @param player das zu speichernde Objekt
+     */
     public void save(Player player) {
-        File file = new File("C:\\spiel\\data.save");
+        File file = new File("C:\\spiel\\data.save"); //Instanz von File wird erzeugt, als Parameter wird
+        //der Ziel-Dateipfad übergeben.
         try {
-            FileOutputStream fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            FileOutputStream fos = new FileOutputStream(file); //Instanz von FileOutputStream, file wird übergeben
+            ObjectOutputStream oos = new ObjectOutputStream(fos); //Instanz von ObjectOutputStream, FileOutputStream
+            //wurde übergeben
 
-            oos.writeObject(player);
+            oos.writeObject(player); //Aktueller Zustand von Player wird von ObjectOutputStream in die Datei geschrieben
 
         } catch (FileNotFoundException notfound) {
             notfound.printStackTrace();
@@ -27,14 +32,18 @@ public class SaveData implements Serializable {
         }
     }
 
+    /**
+     * Methode für das Laden eines zuvor persistent gespeicherten Zustands des Objekts Player.
+     * @return Zustand von player
+     */
     public Player load() {
         Player player = null;
-        File file = new File("C:\\spiel\\data.save");
+        File file = new File("C:\\spiel\\data.save"); //Instanz von File, als Parameter Dateipfad
         try {
-            FileInputStream fis = new FileInputStream(file);
-            ObjectInputStream ois = new ObjectInputStream(fis);
+            FileInputStream fis = new FileInputStream(file); //FileInputStream Instanz bekommt Dateipfad übergeben
+            ObjectInputStream ois = new ObjectInputStream(fis); //ObjectInputStream bekommt FileInputStream übergeben
 
-            player = (Player) ois.readObject();
+            player = (Player) ois.readObject(); //ObjectInputStream wird mit player gleichgesetzt, Zustand "geladen"
 
         } catch (FileNotFoundException notfound) {
             notfound.printStackTrace();
@@ -43,7 +52,7 @@ public class SaveData implements Serializable {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-        return player;
+        return player;  //Geladener Zustand von player wird zurückgegeben.
     }
 
 }
